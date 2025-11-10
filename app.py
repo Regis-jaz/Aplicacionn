9f02c24a54bdb01f8b16b6c5ab37957fc374aff8
Anette.ZL <23308060610457@cetis61.edu.mx>
2025-11-10 11:02:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -28,18 +28,13 @@ def registro():
         nombre = request.form.get('nombre') 
         apellido = request.form.get('apellido')
         email = request.form.get('email') 
-        peso = request.form.get('peso')
-        estatura = request.form.get('estatura')
-        edad = request.form.get('edad')
         preferencia = request.form.get('preferencia')
-        sexo = request.form.get('sexo')
-        fisica = request.form.get('fisica')
         objetivo = request.form.get('objetivo')
         cocina = request.form.get('cocina')
         password = request.form.get('password') 
         confirmPassword = request.form.get('confirmaContraseña') 
 
-        if not all([nombre, apellido, email, peso, estatura, edad, preferencia, sexo, fisica, objetivo, cocina, password, confirmPassword]):
+        if not all([nombre, apellido, email, preferencia, objetivo, cocina, password, confirmPassword]):
             error = 'Todos los campos son obligatorios.'
         if error is None:
             error = f'El correo electrónico "{email}" ya está registrado.'
@@ -53,12 +48,7 @@ def registro():
                 'nombre': nombre,
                 'apellido': apellido,
                 'email': email,
-                'peso': peso,
-                'estatura' : estatura,
-                'edad' : edad,
                 'preferecia' : preferencia,
-                'sexo' : sexo,
-                'fisica' : fisica,
                 'objetivo' : objetivo,
                 'cocina' : cocina,
                 'password': password 
@@ -76,6 +66,16 @@ def recetas():
 def videos():
     return render_template('videos.html')
 
+@app.route('/perfil')
+def perfil():
+    return render_template('perfil.html')
+
+@app.route('/login')
+def login():
+    flash('Has cerrado sesión.', 'info')
+    return redirect(url_for('inicio.html'))
+
+
 @app.route('/descubrete', methods=['GET', 'POST'])
 def descubrete():
     if request.method == 'POST':
@@ -84,9 +84,7 @@ def descubrete():
         edad = int(request.form['edad'])
         genero = request.form['genero']
         actividad = float(request.form['actividad'])
-
         imc = peso / (estatura ** 2)
-
         if imc < 18.5:
             clasificacion = "Bajo peso"
         elif imc < 25:
@@ -95,14 +93,11 @@ def descubrete():
             clasificacion = "Sobrepeso"
         else:
             clasificacion = "Obesidad"
-
         if genero == "masculino":
             tmb = (10 * peso) + (6.25 * (estatura * 100)) - (5 * edad) + 5
         else:
             tmb = (10 * peso) + (6.25 * (estatura * 100)) - (5 * edad) - 161
-
         get = tmb * actividad
-
         return render_template(
             'resultado.html',
             imc=round(imc, 2),
@@ -111,6 +106,7 @@ def descubrete():
             get=round(get, 2)
         )
     return render_template('descubrete.html')
+
 
 @app.route('/resultado', methods=['GET', 'POST'])
 def resultado():
